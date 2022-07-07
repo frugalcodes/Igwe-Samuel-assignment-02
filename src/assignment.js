0dd61a0f0a3aea46812183cc70896d4f0f07c371
Samuel Igwe <samueligwe63@gmail.com>
2022-07-07 20:31:50 +0000

    created a function thats sums items in an array

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,17 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let sum = 0
+  for (let i = 0; i < arrayOfNumbers.length; i += 1) {
+    sum += arrayOfNumbers[i]
+  }
+  
+    return sum;
+  
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
