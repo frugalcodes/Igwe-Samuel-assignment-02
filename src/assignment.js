880d35d69064a67ec0bfc6058602bd39ee58072c
Samuel Igwe <samueligwe63@gmail.com>
2022-07-09 21:00:30 +0000

    i created a function that counts the number of even numbers

--- src/assignment.js
+++ src/assignment.js
@@ -35,9 +35,14 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+  return arrayOfNumbers.filter(item =>
+    (typeof item === 'number') && (item % 2 === 0)
+  ).length
+  
+    
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
